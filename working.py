dc3dde4d4f9d25359767f777e049bff32b9f9f56
npax1 <prakh55555@gmail.com>
2022-10-29 14:14:27 +0300

    add sothing

--- working.py
+++ working.py
@@ -1,4 +1,13 @@
-
-sentence = input('text')
-s=input('s')
-print(sentence.upper(s))
+sentence = '''
+Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Ja
+ne and Michael. My mum enjoys reading and my dad enjoys playing chess with my brother Ken. My mum slim and is rather tall. She has long red hair and 
+'''
+wordstext= input("->")
+words=wordstext.split()
+print(words)
+for word in words:
+    if sentence.count(word)>0:
+        origen=word
+        temp=word.upper()
+        sentence=sentence.replace(origen, temp)
+print(sentence)
